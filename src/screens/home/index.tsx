cbdbc26677219808b913d907b63874ce682169de
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-09 21:39:52 +0700

    slicing home

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,25 +1,19 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import { TodoState } from "../../utils/todo"
 
 import Button from "../../components/Button"
 import TodoList from '../../components/TodoList';
 import Input from '../../components/Input';
+import { palette } from '../../utils/colors/colors';
+
+const { width } = Dimensions.get('window')
 
 const Home = () => {
-    const [todos, setTodos] = useState<TodoState[]>([
-        {
-            id: 1,
-            title: "Playing with dog"
-        },
-        {
-            id: 2,
-            title: "Breakfast"
-        }
-    ]);
-    const [item, setItem] = useState<string>()
+    const [todos, setTodos] = useState<TodoState[]>([]);
+    const [item, setItem] = useState<string>("")
 
     const handleChange = (item: string) => {
         setItem(item)
@@ -30,18 +24,32 @@ const Home = () => {
     };
 
     const handleAddTodo = () => {
-        const newTodo = {
+        const newTodo: TodoState[] = [{
             id: Date.now(),
             title: item,
-        };
-        setTodos(prevTodos => [...prevTodos, newTodo]);
+        }];
+        setTodos(prevTodos => [...prevTodos, ...newTodo]);
+
     };
 
     return (
         <SafeAreaView style={styles.container}>
-            <Input id='addToDo' onChangeText={handleChange} />
-            <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
-            <Button id='submitToDo' title='Add Todo' onPress={handleAddTodo} />
+            <Input id='addToDo' value={item} onChangeText={handleChange} />
+            {
+                todos ?
+                    < TodoList todos={todos} onDeleteTodo={handleDeleteTodo} /> :
+                    <View style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: width / 1.1,
+                    }}>
+                        <Text style={{ margin: 10, color: palette.sage700, fontSize: 18 }}>You don't have any task today!</Text>
+                    </View>
+            }
+            <View style={styles.buttonContainer}>
+                <Button id='submitToDo' title='Add Todo' onPress={handleAddTodo} />
+            </View>
         </SafeAreaView>
     )
 }
@@ -53,5 +61,11 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 20,
         backgroundColor: '#fff',
+    },
+    buttonContainer: {
+        width: width / 1.2,
+        position: 'absolute',
+        bottom: 20,
+        alignSelf: 'center',
     },
 })
