23999bb2db43c34dddcfecc0c72431ea38626350
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-16 11:52:10 +0700

    setup storage

---
+++ src/utils/services/storage.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const setItemWithExpiry = async (
+  key: any,
+  value: number | string | [] | {},
+  expiryInMinutes: number
+) => {
+  const item = {
+    value,
+    expiry: Date.now() + expiryInMinutes * 60 * 1000,
+  };
+  await AsyncStorage.setItem(key, JSON.stringify(item));
+};
+
+const getItemWithExpiry = async (key: any) => {
+  const itemString = await AsyncStorage.getItem(key);
+  if (!itemString) {
+    return null;
+  }
+
+  const item = JSON.parse(itemString);
+  const now = Date.now();
+  if (now > item.expiry) {
+    await AsyncStorage.removeItem(key);
+    return null;
+  }
+
+  return item.value;
+};
+
+export default { setItemWithExpiry, getItemWithExpiry };
