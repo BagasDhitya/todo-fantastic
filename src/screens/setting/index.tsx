7437ba67e9b81513b25204f8c944943ad2483684
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-06-19 18:05:47 +0700

    apply zustand on language

--- src/screens/setting/index.tsx
+++ src/screens/setting/index.tsx
@@ -1,90 +1,103 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Switch, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { StyleSheet, Text, View, Switch, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import { useLanguageStore } from "../../utils/zustand/languageState";
 
-import { palette } from '../../utils/colors/colors';
-import { removeItem } from '../../utils/services/storage';
+import { palette } from "../../utils/colors/colors";
+import { removeItem } from "../../utils/services/storage";
 
-import Navbar from '../../components/Navbar';
+import Navbar from "../../components/Navbar";
 
 const Setting = () => {
+  const navigation: any = useNavigation();
+  const { language, setLanguage } = useLanguageStore();
 
-    const navigation: any = useNavigation()
-    const [isEnglish, setIsEnglish] = useState(false);
+  const handleLogout = () => {
+    removeItem("token");
+    navigation.navigate("Login");
+  };
 
-    const handleLanguageToggle = () => {
-        setIsEnglish(!isEnglish);
-    };
+  const toggleLanguage = () => {
+    const newLanguage = language === "English" ? "Indonesia" : "English";
+    setLanguage(newLanguage);
+  };
 
-    const handleLogout = () => {
-        removeItem("token")
-        navigation.navigate("Login")
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Navbar />
-            <View style={styles.content}>
-                <TouchableOpacity style={styles.item} onPress={handleLogout}>
-                    <Text style={styles.itemText}>About</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.item} onPress={handleLanguageToggle}>
-                    <View style={styles.languageContainer}>
-                        <Text style={styles.itemText}>Language:</Text>
-                        <Text style={styles.languageText}>{isEnglish ? 'English' : 'Indonesia'}</Text>
-                    </View>
-                    <Switch thumbColor={palette.sage400} value={isEnglish} onValueChange={handleLanguageToggle} />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.item} onPress={handleLogout}>
-                    <Text style={styles.logoutText}>Logout</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navbar />
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.item} onPress={handleLogout}>
+          <Text style={styles.itemText}>
+            {language === "Indonesia" ? "Tentang" : "About"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item} onPress={toggleLanguage}>
+          <View style={styles.languageContainer}>
+            <Text style={styles.itemText}>
+              {" "}
+              {language === "Indonesia" ? "Bahasa" : "Language"}
+            </Text>
+            <Text style={styles.languageText}>{language}</Text>
+          </View>
+          <Switch
+            thumbColor={palette.sage400}
+            value={language === "English"}
+            onValueChange={toggleLanguage}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.item} onPress={handleLogout}>
+          <Text style={styles.logoutText}>
+            {" "}
+            {language === "Indonesia" ? "Keluar" : "Logout"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: "#fff",
-    },
-    content: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    item: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-    },
-    itemText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: palette.sage600
-    },
-    languageContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    languageText: {
-        marginLeft: 5,
-        fontWeight: 'bold',
-        color: palette.sage700
-    },
-    logoutText: {
-        color: 'red',
-        fontWeight: 'bold',
-    },
-    switch: {
-        color: palette.sage700
-    }
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: palette.sage600,
+  },
+  languageContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  languageText: {
+    marginLeft: 5,
+    fontWeight: "bold",
+    color: palette.sage700,
+  },
+  logoutText: {
+    color: "red",
+    fontWeight: "bold",
+  },
+  switch: {
+    color: palette.sage700,
+  },
 });
 
 export default Setting;
